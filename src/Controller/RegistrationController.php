fabcc15a289a2dd1581079b528472cff0a7df55b
Anais CEN <anaiscen@gmail.com>
2021-01-03 16:01:27 +0100

    Add forgot password

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\NewPasswordType;
+use App\Form\PasswordForgotType;
 use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
 use App\Security\EmailVerifier;
@@ -99,6 +101,88 @@ class RegistrationController extends AbstractController
         {
             $this->addFlash('verify_email_error','Erreur lors de la validation du compte');
             return $this->redirectToRoute('app_register');
+        }
+    }
+
+    /**
+     * @Route("/passwordforgot", name="app_password_forgot")
+     * @param Request $request
+     * @param UserRepository $userRepo
+     * @param MailerInterface $mailer
+     * @return Response
+     */
+    public function passwordForgot(Request $request,UserRepository $userRepo, MailerInterface $mailer): Response
+    {
+
+        $form = $this->createForm(PasswordForgotType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $userRepo->findOneByUserName($form->get('username')->getData());
+
+            $user->setToken(md5(random_bytes(10)));
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $email = new TemplatedEmail();
+            $email->subject('SnowTricks - Reset password')
+                ->to($user->getEmail())
+                ->from('eee45ee559-c5b5f5@inbox.mailtrap.io')
+                ->html(
+                    $this->renderView('registration/password_forgot_email.html.twig', [
+                        'user' => $user
+                    ]),
+                    'text/html'
+                );
+            $mailer->send($email);
+            return $this->redirectToRoute('trick');
+        }
+
+        return $this->render('registration/password_forgot.html.twig', [
+            'passwordForgotForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/newpassword/{username}/{token}", name="app_new_password")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param string $username
+     * @param string $token
+     * @param UserRepository $userRepo
+     * @return Response
+     */
+    public function newPassword(Request $request, UserPasswordEncoderInterface $passwordEncoder,string $username, string $token, UserRepository $userRepo): Response
+    {
+        $user = $userRepo->findOneByUserName($username);
+        $form = $this->createForm(NewPasswordType::class,$user);
+        $form->handleRequest($request);
+
+        // Check token validity
+        if(!$user || $user->getToken() != $token) {
+            $this->addFlash('verify_email_error','The link is not valid');
+            return $this->redirectToRoute('app_password_forgot');
+        }
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+        else {
+            return $this->render('registration/new_password.html.twig', [
+                'newPasswordForm' => $form->createView(), ['user' => $user]
+            ]);
         }
     }
 }
---
+++ src/Form/NewPasswordType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\EqualTo;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class NewPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('userName',HiddenType::class)
+            ->add('token',HiddenType::class)
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veillez saisir un mot de passe valide.',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères.',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            /*'data_class' => User::class,*/
+        ]);
+    }
+}
